0c5dfb542420243a7a2f34d2495979a7e506419c
Jurzo <juuso.kalliomaki96@gmail.com>
2017-03-11 20:37:49 +0200

    Create tietorakenne.h
    tietorakenne header

---
+++ tietorakenne.h
@@ -0,0 +1,17 @@
+typedef struct tuote {
+	float koko;
+	float arvo;
+	char *nimike;
+	struct tuote *pSeuraava;
+} Tuote;
+
+typedef struct palautus {
+	char *aikaleima;
+	struct tuote *pTuote;
+	struct palautus *pSeuraava;
+} Palautus;
+
+int lisaaTuote(Tuote **alku, float tArvo, float tKoko, char *puskuri);
+int lisaaPalautus(Palautus **alku, Tuote *tuote, char *puskuri);
+void vapautaTuotteet(Tuote *p);
+void vapautaPalautukset(Palautus *p);
