1918270c71e5e813862b448307aa4d15f2c20eda
jPuddle <mikukoyutira@gmail.com>
2017-03-11 21:39:12 +0200

    Update tiedosto.h

--- tiedosto.h
+++ tiedosto.h
@@ -1,3 +1,6 @@
+#ifndef TIEDOSTO_H
+#define TIEDOSTO_H
 int lueNimikkeet(char *, Tuote **);
 int kirjoitaTilapainenLoki(char *, Palautus *);
 int kirjoitaLoki(char *tiedostonimi, Palautus *alku);
+#endif
