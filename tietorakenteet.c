835b1ee94bdba2c31e3fc499891f313d01b648f3
DarkNilb <kopsamika@gmail.com>
2017-03-11 21:46:14 +0200

    Update tietorakenteet.c

--- tietorakenteet.c
+++ tietorakenteet.c
@@ -1,4 +1,4 @@
-//Nimet: Mika Kopsa 0507014, Juuso Kalliomäki 0457960, Mikko Jutila 0447734
+//Mika Kopsa 0507014, Juuso Kalliomäki 0457960, Mikko Jutila 0447734
 
 #include <stdlib.h>
 #include <string.h>
